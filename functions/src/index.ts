676eb5c752aa31a489c790712c4d02c03e8a346f
Grace Wang <gracewang92@gmail.com>
2020-07-03 16:43:40 -0700

    simple database access

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,14 @@
-import * as functions from 'firebase-functions';
+import * as functions from "firebase-functions";
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
-//
-export const typeformComplete = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
-});
+const admin = require("firebase-admin");
+admin.initializeApp();
+
+export const registerUser = functions.https.onRequest(
+  async (request, response) => {
+    const { blob, ...other } = request.body;
+    await admin.firestore().collection("users").doc().set(other);
+    response.send("Done");
+  }
+);
