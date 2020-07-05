5f54315906bcdf439ac3d739a3efb0dfd65367c7
Sachith Gullapalli <sgullapalli@rippling.com>
2020-07-05 00:49:07 -0400

    setup twilio

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,6 +2,7 @@
 // // https://firebase.google.com/docs/functions/typescript
 import * as admin from "firebase-admin";
 import * as functions from "firebase-functions";
+import { getConferenceTwimlForPhone, callNumberWithTwiml } from "./twilio";
 
 admin.initializeApp();
 
@@ -13,6 +14,24 @@ export const registerUser = functions.https.onCall(
     await ref.set(user);
     return user;
   }
+);
+
+export const callUser = functions.https.onCall(
+    async (request) => {
+        const ref = admin.firestore().collection("users").doc(request.user_id);
+        const user_doc = await ref.get();
+        const user_doc_data = user_doc.data()
+        if (!user_doc.exists || !user_doc_data) {
+            return { "error": "User does not exist!"};
+        }
+        const phone = user_doc_data.phone_number;
+        const twiml = await getConferenceTwimlForPhone(phone, true);
+        if (!twiml) {
+            return { "error": "User has no current matches!"};
+        }
+        await callNumberWithTwiml(phone, twiml);
+        return {"success": "Calling " + phone};
+      }
 );
 
 export const optIn = functions.https.onRequest(
@@ -35,16 +54,17 @@ export const optIn = functions.https.onRequest(
             response.send({success: true});
         }
     }
-)
+);
 
 export const reveal = functions.https.onRequest(
     async (request, response) => {
         const match_id = request.body.match_id;
         const phone_number = request.body.phone_number;
-        
+
         const match_doc_ref = admin.firestore().collection("matches").doc(match_id);
         const match_doc = await match_doc_ref.get();
-        if (!match_doc.exists) {
+        const match_doc_data = match_doc.data();
+        if (!match_doc.exists || !match_doc_data) {
             console.log("ERROR | No match with id " + match_id);
             response.send({success: false, message: "Match does not exist"});
         } else {
@@ -63,11 +83,11 @@ export const reveal = functions.https.onRequest(
             console.log("Reveailing user id " + user_a_id);
 
 
-            if (match_doc.data().user_a_id == user_a_id) {
-                match_doc_ref.update({user_a_revealed: true});
+            if (match_doc_data.user_a_id === user_a_id) {
+                await match_doc_ref.update({user_a_revealed: true});
                 response.send({success: true});
-            } else if (match_doc.data().user_b_id == user_a_id) {
-                match_doc_ref.update({user_b_revealed: true});
+            } else if (match_doc_data.user_b_id === user_a_id) {
+                await match_doc_ref.update({user_b_revealed: true});
                 response.send({success: true});
             } else {
                 // match doesn't have the users in request
@@ -76,12 +96,24 @@ export const reveal = functions.https.onRequest(
             }
         }
     }
-)
+);
+
+export const addUserToCall = functions.https.onRequest(
+    async (request, response) => {
+        const caller_phone = request.body.From;
+        const twiml = await getConferenceTwimlForPhone(caller_phone, false);
+        response.set('Content-Type', 'text/xml');
+        response.send(twiml.toString());
+    }
+);
+
+
+
 
 export const getUsers = functions.https.onRequest(
   async (request, response) => {
     const users = await admin.firestore().collection("users").get();
-    
+
     response.send(users.docs.map((user) => user.id ));
   }
 );
---
+++ functions/src/twilio.ts
@@ -0,0 +1,58 @@
+const VoiceResponse = require('twilio').twiml.VoiceResponse;
+import * as admin from "firebase-admin";
+
+const TWILIO_NUMBER = '+12036338466';
+const accountSid = 'AC07d4a9a61ac7c91f7e5cecf1e27c45a6';
+const authToken = 'e4cac763ca2438390561cb3b1c2f6b72';
+const client = require('twilio')(accountSid, authToken);
+
+
+export const getConferenceTwimlForPhone = async (phone_number: string, null_on_error=true) => {
+    const users = admin.firestore().collection("users");
+    const stripped_number = phone_number.replace("+", "");
+    const result = await users.where("phone_number", "==", stripped_number).get();
+    let error_response = null;
+    if (!null_on_error) {
+        error_response = new VoiceResponse();
+        error_response.say({
+            'voice': 'alice',
+        }, "We don't have a match for you!  Please try again later.");
+    }
+
+    if (result.empty) {
+        console.log("ERROR | No user with phone number " + phone_number);
+        return error_response;
+    }
+    console.log("Finding conference for user with phone number " + phone_number);
+    const user_id = result.docs[0].id;
+    const matches = admin.firestore().collection("matches");
+    const match_a_result = await matches.where("user_a_id", "==", user_id).orderBy("created_at", "desc").limit(1).get();
+    const match_b_result = await matches.where("user_b_id", "==", user_id).orderBy("created_at", "desc").limit(1).get();
+    let match_result = null;
+    if (!match_a_result.empty) {
+        match_result = match_a_result.docs[0];
+    }
+    if (!match_b_result.empty) {
+        const match_b_result_doc = match_b_result.docs[0];
+        if (!match_result || (match_result.data()['created_at']  < match_b_result_doc.data()['created_at'])) {
+            match_result = match_b_result_doc;
+        }
+    }
+    if (!match_result) {
+        return error_response;
+    }
+    const twiml = new VoiceResponse();
+    const dial = twiml.dial();
+    dial.conference(match_result.id);
+    return twiml;
+}
+
+export const callNumberWithTwiml = async (number: string, twiml: any) => {
+    client.calls
+      .create({
+         twiml: twiml.toString(),
+         to: number,
+         from: TWILIO_NUMBER
+       })
+      .then((call: { sid: any; }) => console.log(call.sid));
+}
