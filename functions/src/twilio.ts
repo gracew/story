4ab1de81528be211824488f46371f8ffcadd0b67
Sachith Gullapalli <sgullapalli@rippling.com>
2020-08-02 21:41:39 -0700

    jitterbuffer off

--- functions/src/twilio.ts
+++ functions/src/twilio.ts
@@ -42,7 +42,7 @@ export const getConferenceTwimlForPhone = async (phone_number: string, null_on_e
     }
     const twiml = new VoiceResponse();
     const dial = twiml.dial();
-    dial.conference(match_result.id, {jitterBufferSize: 'small'});
+    dial.conference(match_result.id, {jitterBufferSize: 'off'});
     return twiml;
 }
 
