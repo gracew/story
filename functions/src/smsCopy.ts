aa3fecd6b7693d1873958269652544b29e8eb12e
Grace Wang <gracewang92@gmail.com>
2020-09-27 17:50:56 -0700

    minor linting, copy fun facts script

--- functions/src/smsCopy.ts
+++ functions/src/smsCopy.ts
@@ -1,8 +1,8 @@
 import * as moment from "moment-timezone";
 import { IMatch, IUser } from "./firestore";
 
-export function availability(user: IUser, timezone: string) {
-    return `Hi ${user.firstName}. It's Voicebar. We've got a potential match for you! Are you available for a 30 minute phone call with your match at 8pm ${timezone} any day this week? Please respond with all the days you're free. You can also reply SKIP to skip this week. Respond in the next 3 hours to confirm your date.`;
+export function availability(user: IUser, tz: string) {
+    return `Hi ${user.firstName}. It's Voicebar. We've got a potential match for you! Are you available for a 30 minute phone call with your match at 8pm ${tz} any day this week? Please respond with all the days you're free. You can also reply SKIP to skip this week. Respond in the next 3 hours to confirm your date.`;
 }
 
 export function matchNotification(userId: string, matches: IMatch[], usersById: Record<string, IUser>): string[] {
@@ -28,28 +28,28 @@ Happy chatting!`
             );
         }
         return texts;
+    } else {
+        const match1 = matches[0];
+        const matchUser1Id = match1.user_a_id === userId ? match1.user_b_id : match1.user_a_id;
+        const match1User = usersById[matchUser1Id];
+        const match2 = matches[1];
+        const matchUser2Id = match2.user_a_id === userId ? match2.user_b_id : match2.user_a_id;
+        const match2User = usersById[matchUser2Id];
+        const texts = [
+            `Hi ${user.firstName}, we have two Voicebar matches for you! On ${day(match1)} you'll be chatting with ${match1User.firstName} and on ${day(match2)} you'll be chatting with ${match2User.firstName}. At 8pm ${tz} both nights you’ll receive a phone call connecting you with your match. ${phoneSwapText}`
+        ];
+        if (user.funFacts && match1User.funFacts) {
+            texts.push(
+                `Here are a few fun facts about ${match1User.firstName}: "${match1User.funFacts}"`
+            );
+        }
+        if (user.funFacts && match2User.funFacts) {
+            texts.push(
+                `Here are a few fun facts about ${match2User.firstName}: "${match2User.funFacts}"`
+            );
+        }
+        return texts;
     }
-    // TODO(gracew): handle multiple match case
-    const match1 = matches[0];
-    const matchUser1Id = match1.user_a_id === userId ? match1.user_b_id : match1.user_a_id;
-    const match1User = usersById[matchUser1Id];
-    const match2 = matches[1];
-    const matchUser2Id = match2.user_a_id === userId ? match2.user_b_id : match2.user_a_id;
-    const match2User = usersById[matchUser2Id];
-    const texts = [
-        `Hi ${user.firstName}, we have two Voicebar matches for you! On ${day(match1)} you'll be chatting with ${match1User.firstName} and on ${day(match2)} you'll be chatting with ${match2User.firstName}. At 8pm ${tz} both nights you’ll receive a phone call connecting you with your match. ${phoneSwapText}`
-    ];
-    if (user.funFacts && match1User.funFacts) {
-        texts.push(
-            `Here are a few fun facts about ${match1User.firstName}: "${match1User.funFacts}"`
-        );
-    }
-    if (user.funFacts && match2User.funFacts) {
-        texts.push(
-            `Here are a few fun facts about ${match2User.firstName}: "${match2User.funFacts}"`
-        );
-    }
-    return texts;
 }
 
 function timezone(match: IMatch) {
