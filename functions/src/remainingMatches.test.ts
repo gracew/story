37ce76527b5d1b083b0488ac9dcfb34dddc3c280
Grace Wang <gracewang92@gmail.com>
2020-10-03 17:37:16 -0700

    rm test file

--- functions/src/remainingMatches.test.ts
+++
@@ -1,6 +0,0 @@
-import { generateRemainingMatchCount } from "./remainingMatches";
-
-it("test airtable", async () => {
-    const res = await generateRemainingMatchCount();
-    console.log(res);
-});
