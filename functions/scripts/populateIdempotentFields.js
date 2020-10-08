63ca23d6401a8e5d85e03d9fa7491c4beda5f511
Grace Wang <gracewang92@gmail.com>
2020-10-07 22:05:09 -0700

    delete comment

--- functions/scripts/populateIdempotentFields.js
+++ functions/scripts/populateIdempotentFields.js
@@ -1,12 +1,3 @@
-/*
-Usage:
-GOOGLE_APPLICATION_CREDENTIALS=<path to json file> yarn node scripts/validateUsers.js
-
-Checks the following:
-- each user has the fields firstName, phone
-- phone starts with a +, followed by at least 1 digit 0-9
-*/
-
 const admin = require("firebase-admin");
 const util = require("./util");
 const moment = require('moment');
