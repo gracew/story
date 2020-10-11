33f5229c514c2bb411814b5a3d9342c2203a9882
Grace Wang <gracewang92@gmail.com>
2020-10-10 21:00:58 -0700

    add to validate matches script

--- functions/scripts/validateMatches.js
+++ functions/scripts/validateMatches.js
@@ -3,13 +3,12 @@ Usage:
 GOOGLE_APPLICATION_CREDENTIALS=<path to json file> yarn node scripts/validateMatches.js
 
 Checks the following:
-- each match has the fields user_ids, user_a_id, user_b_id
+- each match has the fields id, user_ids, user_a_id, user_b_id, reminded, called, warned5Min, warned1Min, revealRequested
 - user_ids is of size 2 and contains both user_a_id and user_b_id
 - the user_ids map to documents in the users collection
 */
 
 const admin = require("firebase-admin");
-const moment = require("moment-timezone");
 const util = require("./util");
 
 util.checkRequiredEnvVars(["GOOGLE_APPLICATION_CREDENTIALS"]);
@@ -19,12 +18,24 @@ admin.initializeApp();
 admin
   .firestore()
   .collection("matches")
+  .where("created_at", ">=", new Date("2020-10-08T07:00:00.000Z"))
   .get()
   .then(async (res) => {
     var invalidCount = 0;
     const allUserRefs = [];
 
     res.docs.forEach((doc) => {
+      if (!doc.get("id") ||
+        doc.get("reminded") === undefined ||
+        doc.get("called") === undefined ||
+        doc.get("warned5Min") === undefined ||
+        doc.get("warned1Min") === undefined ||
+        doc.get("revealRequested") === undefined) {
+        invalidCount++;
+        console.log(doc.id);
+        return;
+      }
+
       const userIds = doc.get("user_ids");
       const userAId = doc.get("user_a_id");
       const userBId = doc.get("user_b_id");
