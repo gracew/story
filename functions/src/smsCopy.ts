e123fa3f02af005def3330301a351996c2bdd7fc
Grace Wang <gracewang92@gmail.com>
2020-10-19 23:59:35 -0700

    swap out prompt this week

--- functions/src/smsCopy.ts
+++ functions/src/smsCopy.ts
@@ -73,7 +73,7 @@ function day(match: IMatch) {
 }
 
 export function reminder(userA: IUser, userB: IUser) {
-    const prompt = "What are the open tabs on your phone?";
+    const prompt = "What’s the most obscure subject that you know a lot about?";
     return `Hi ${userA.firstName}! This is Voicebar. Just a reminder that you’ll be speaking with ${userB.firstName} in an hour. Here's one idea to get the conversation started: "${prompt}" Hope you two have a good date!`;
 }
 
