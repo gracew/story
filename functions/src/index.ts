56ff3d4c2138f3886916fe2528b89f6334c42439
Grace Wang <gracewang92@gmail.com>
2020-11-17 14:58:50 -0800

    go back to robot voice for now (#73)
    * remove intro/outro recordings
    
    * Revert "use bell sound at 5m and 1m mark"
    
    This reverts commit d7ff7f8491be2237f6bb492c82b7144b6c8faf20.

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -348,11 +348,6 @@ export const revealRequest = functions.pubsub.schedule('0,30 * * * *').onRun(asy
 async function playCallOutro(match: IMatch, conferenceSid: string) {
     try {
         // wrap in try/catch as twilio will throw if the conference has already ended
-        const participants = await client.conferences(conferenceSid).participants.list();
-        await Promise.all(participants.map(participant =>
-            client.conferences(conferenceSid).participants(participant.callSid).update({ muted: true })))
-        await client.conferences(conferenceSid).update({ announceUrl: BASE_URL + "callOutro" })
-        await util.promisify(setTimeout)(30_000);
         await client.conferences(conferenceSid).update({ status: "completed" })
     } catch (err) {
         console.log(err);
@@ -410,7 +405,7 @@ export const screenCall = functions.https.onRequest(
     async (request, response) => {
         const twiml = new twilio.twiml.VoiceResponse();
         const gather = twiml.gather({ numDigits: 1, action: BASE_URL + "addUserToCall" });
-        gather.play("https://firebasestorage.googleapis.com/v0/b/speakeasy-prod.appspot.com/o/callSounds%2Fvoicebar_screen.mp3?alt=media");
+        gather.say({ voice: "alice" }, 'Welcome! Press any key to enter your voice date.');
 
         // If the user doesn't enter input, loop
         twiml.redirect('/screenCall');
@@ -451,19 +446,10 @@ export const conferenceStatusWebhook = functions.https.onRequest(
     }
 );
 
-export const announceUser = functions.https.onRequest(
-    (request, response) => {
-        const twiml = new twilio.twiml.VoiceResponse();
-        twiml.play("https://firebasestorage.googleapis.com/v0/b/speakeasy-prod.appspot.com/o/callSounds%2Fvoicebar_intro.mp3?alt=media");
-        response.set('Content-Type', 'text/xml');
-        response.send(twiml.toString());
-    }
-);
-
 export const announce5Min = functions.https.onRequest(
     (request, response) => {
         const twiml = new twilio.twiml.VoiceResponse();
-        twiml.play("https://firebasestorage.googleapis.com/v0/b/speakeasy-prod.appspot.com/o/callSounds%2Fbell.mp3?alt=media");
+        twiml.say({ 'voice': 'alice' }, "Your call will end in 5 minutes.");
         response.set('Content-Type', 'text/xml');
         response.send(twiml.toString());
     }
@@ -472,16 +458,7 @@ export const announce5Min = functions.https.onRequest(
 export const announce1Min = functions.https.onRequest(
     (request, response) => {
         const twiml = new twilio.twiml.VoiceResponse();
-        twiml.play("https://firebasestorage.googleapis.com/v0/b/speakeasy-prod.appspot.com/o/callSounds%2Fbell.mp3?alt=media");
-        response.set('Content-Type', 'text/xml');
-        response.send(twiml.toString());
-    }
-);
-
-export const callOutro = functions.https.onRequest(
-    (request, response) => {
-        const twiml = new twilio.twiml.VoiceResponse();
-        twiml.play("https://firebasestorage.googleapis.com/v0/b/speakeasy-prod.appspot.com/o/callSounds%2Fvoicebar_outro.mp3?alt=media");
+        twiml.say({ 'voice': 'alice' }, "Your call will end in 1 minute.");
         response.set('Content-Type', 'text/xml');
         response.send(twiml.toString());
     }
