d50a514e7218d89ff35b62ba00def77bea2020c9
Grace Wang <gracewang92@gmail.com>
2020-11-29 23:22:25 -0800

    update registerUser, sendgrid confirmation email (#76)

--- functions/src/sendgrid.ts
+++ functions/src/sendgrid.ts
@@ -1,19 +1,28 @@
 import * as functions from "firebase-functions";
+import * as sendgrid from "@sendgrid/mail";
 
-const client = require('@sendgrid/client');
-client.setApiKey(functions.config().sendgrid.key);
+sendgrid.setApiKey(functions.config().sendgrid.key);
 
+export async function sendConfirmationEmail(user: any) {
+  const welcomeEmailText = `Hi ${user.firstName},
 
-//Adds user to Sendgrid marketing email list
-export const addUsersToSendgrid = (userData : {age: string, email: string, first_name: string, last_name: string}[]) =>{
-    let request = {
-        body: userData,
-        method: 'POST',
-        url: '/v3/contactdb/recipients'
+Thanks for your interest in Story Dating! We are a personalized matchmaking service that helps you meet new people, have interesting conversations, and form genuine connections.
+
+We are currently limiting the number of new users so that we can focus on providing high-quality, curated matches. We'll add you to our waitlist and notify you when a spot opens up. In the meantime, you can get off the waitlist sooner by referring your friends using this personalized link: https://storydating.com/join?r=${user.id}.
+
+- Grace from Story Dating
+`
+
+  const msg = {
+    to: user.email,
+    from: 'hello@storydating.com',
+    subject: 'Welcome to Story Dating!',
+    text: welcomeEmailText,
+    trackingSettings: {
+      clickTracking: {
+        enable: false,
+      }
     }
-    client.request(request)
-    .then(([response, body] : [any, any]) => {
-      console.log(response.statusCode);
-      console.log(response.body);
-    })
+  }
+  await sendgrid.send(msg)
 }
