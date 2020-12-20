634731bde9f4a60b54de6be98fa5322440f7fd30
Grace Wang <gracewang92@gmail.com>
2020-12-20 14:56:29 -0800

    script updates

---
+++ functions/scripts/responseReminder.js
@@ -0,0 +1,20 @@
+const admin = require("firebase-admin");
+const moment = require('moment');
+const util = require("./util");
+
+util.checkRequiredEnvVars(["GOOGLE_APPLICATION_CREDENTIALS"]);
+
+admin.initializeApp();
+
+const week = moment().startOf("week").format("YYYY-MM-DD");
+admin
+  .firestore()
+  .collection("scheduling").doc(week).collection("users")
+  .get()
+  .then(async (res) => {
+    const ids = res.docs.filter(doc => Object.keys(doc.data()).length === 0).map(doc => admin.firestore().collection("users").doc(doc.id));
+    const users = await admin.firestore().getAll(...ids);
+    users.forEach(u => {
+      console.log(`${u.get("phone")},"Your match will expire soon! If you'd like to connect this week, please reply in the next hour and let us know which days work for you for an 8PM ${u.get("timezone")} call."`);
+    })
+  });
