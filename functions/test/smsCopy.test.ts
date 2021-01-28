05820ed7af9ccebcdbeddf267b5527f5a78586f7
Grace Wang <gracewang92@gmail.com>
2021-01-28 13:53:14 -0800

    fixes to location in match notifications

--- functions/test/smsCopy.test.ts
+++ functions/test/smsCopy.test.ts
@@ -7,7 +7,7 @@ const userId2 = uuid.v4();
 const userId3 = uuid.v4();
 const expectedSimpleET = "Hi Anna, on Wednesday you'll be chatting with Grace. At 8:00pm EDT";
 const expectedSimple = "Hi Anna, on Wednesday you'll be chatting with Grace. At 8:00pm PDT";
-const expectedSimpleNYC = "Hi Anna, on Wednesday you'll be chatting with Grace from New York City. At 8:00pm PDT";
+//const expectedSimpleNYC = "Hi Anna, on Wednesday you'll be chatting with Grace from New York City. At 8:00pm PDT";
 const expectedSimpleSF = "Hi Anna, on Wednesday you'll be chatting with Grace from the San Francisco Bay Area. At 8:00pm PDT";
 const expectedTwoMatches = "Hi Anna, we have two matches for you! On Wednesday you'll be chatting with Grace and on Thursday you'll be chatting with Rachael. At 8:00pm PDT both nights";
 const expectedTwoMatchesSameLocation = "Hi Anna, we have two matches for you! On Wednesday you'll be chatting with Grace and on Thursday you'll be chatting with Rachael. They are both from New York City. At 8:00pm PDT both nights";
@@ -71,16 +71,16 @@ Happy chatting!`)
 });
 
 it("matchNotification for a single match - location", async () => {
-    const user1 = user("Anna");
+    const user1 = user("Anna", undefined, "San Francisco Bay Area", false);
     const user2 = user("Grace", undefined, "New York City", true);
     const m = match(userId1, userId2, "2020-09-23T20:00:00-07:00");
     const res = matchNotification(userId1, [m], { [userId1]: user1, [userId2]: user2 })
     expect(res).toHaveLength(1);
-    expect(res[0]).toContain(expectedSimpleNYC)
+    expect(res[0]).toContain(expectedSimple)
 });
 
 it("matchNotification for a single match - location SF", async () => {
-    const user1 = user("Anna");
+    const user1 = user("Anna", undefined, "San Francisco Bay Area", true);
     const user2 = user("Grace", undefined, "San Francisco Bay Area", true);
     const m = match(userId1, userId2, "2020-09-23T20:00:00-07:00");
     const res = matchNotification(userId1, [m], { [userId1]: user1, [userId2]: user2 })
@@ -126,6 +126,17 @@ it("matchNotification for two matches - same location", async () => {
     const user1 = user("Anna", undefined, "location", true);
     const user2 = user("Grace", undefined, "New York City", true);
     const user3 = user("Rachael", undefined, "New York City", true);
+    const matchUser2 = match(userId1, userId2, "2020-09-23T20:00:00-07:00");
+    const matchUser3 = match(userId1, userId3, "2020-09-24T20:00:00-07:00");
+    const res = matchNotification(userId1, [matchUser2, matchUser3], { [userId1]: user1, [userId2]: user2, [userId3]: user3 })
+    expect(res).toHaveLength(1);
+    expect(res[0]).toContain(expectedTwoMatchesSameLocation);
+});
+
+it("matchNotification for two matches - others not flexible", async () => {
+    const user1 = user("Anna", undefined, "New York City", true);
+    const user2 = user("Grace", undefined, "New York City", false);
+    const user3 = user("Rachael", undefined, "New York City", false);
     const matchUser2 = match(userId1, userId2, "2020-09-23T20:00:00-07:00");
     const matchUser3 = match(userId1, userId3, "2020-09-24T20:00:00-07:00");
     const res = matchNotification(userId1, [matchUser2, matchUser3], { [userId1]: user1, [userId2]: user2, [userId3]: user3 })
