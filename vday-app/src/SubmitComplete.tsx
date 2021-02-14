541500d5140b01aa687f563afbd364f3abc21f6e
Grace Wang <gracewang92@gmail.com>
2021-02-13 20:00:38 -0800

    back to homepage button lol

--- vday-app/src/SubmitComplete.tsx
+++ vday-app/src/SubmitComplete.tsx
@@ -2,12 +2,13 @@ import { Button } from "antd";
 import "firebase/analytics";
 import "firebase/functions";
 import React from "react";
-import { useLocation } from "react-router-dom";
+import { useHistory, useLocation } from "react-router-dom";
 import "./SubmitComplete.css";
 
 function SubmitComplete() {
     const query = new URLSearchParams(useLocation().search);
     const firstName = query.get("name");
+    const history = useHistory();
 
     return (
         <div className="vday-record">
@@ -15,7 +16,7 @@ function SubmitComplete() {
                 <h2>Thanks so much, {firstName}!</h2>
                 <p>We can't wait to hear about your experience with dating apps. If we love it we'll feature it on our homepage or on our social accounts!</p>
                 <p>If the current dating apps aren't doing it for you, give <a href="https://storydating.com">Story Dating</a> a try: it's a voice-first experience that focuses on connection over personality.</p>
-                <Button className="back-to-homepage">Back to homepage</Button>
+                <Button className="back-to-homepage" onClick={() => history.push("/")}>Back to homepage</Button>
                 <p>Questions? Contact <a href="mailto:hello@storydating.com">hello@storydating.com</a></p>
             </div>
         </div>
