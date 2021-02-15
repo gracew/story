4956089d9f68fd8f7512d41b40ab0c8eb3438054
Grace Wang <gracewang92@gmail.com>
2021-02-14 16:21:12 -0800

    include SO link for email validation

--- vday-app/src/Recorder.tsx
+++ vday-app/src/Recorder.tsx
@@ -83,6 +83,7 @@ function Recorder() {
     }
   }
 
+  // https://stackoverflow.com/questions/46155/how-to-validate-an-email-address-in-javascript
   function validEmail(email: string) {
     const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(email.toLowerCase());
