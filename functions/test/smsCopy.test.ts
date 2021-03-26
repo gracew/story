b75363dd3f8f4ae6137412ccf38b9155974b0d56
Grace Wang <gracewang92@gmail.com>
2021-03-25 20:57:21 -0700

    improve SMS copy (#137)
    * improve SMS copy
    
    * use story.dating domain in texts
    
    * netlify redirect of story.dating

--- functions/test/smsCopy.test.ts
+++ functions/test/smsCopy.test.ts
@@ -5,11 +5,21 @@ import { match, user } from "./mock";
 const userId1 = uuid.v4();
 const userId2 = uuid.v4();
 const userId3 = uuid.v4();
-const expectedSimpleET = "Hi Anna, on Wednesday you'll be chatting with Grace from the San Francisco Bay Area. At 8:00pm EDT";
-const expectedSimple = "Hi Anna, on Wednesday you'll be chatting with Grace from the San Francisco Bay Area. At 8:00pm PDT";
-const expectedTwoMatches = "Hi Anna, we have two matches for you! On Wednesday you'll be chatting with Grace and on Thursday you'll be chatting with Rachael. They are both from the San Francisco Bay Area. At 8:00pm PDT both nights";
-const expectedTwoMatchesSameLocation = "Hi Anna, we have two matches for you! On Wednesday you'll be chatting with Grace and on Thursday you'll be chatting with Rachael. They are both from New York City. At 8:00pm PDT both nights";
-const expectedTwoMatchesDiffLocation = "Hi Anna, we have two matches for you! On Wednesday you'll be chatting with Grace from New York City and on Thursday you'll be chatting with Rachael from the San Francisco Bay Area. At 8:00pm PDT both nights";
+const expectedSimpleET = `Hi Anna, you've got a match! On Wednesday you'll be chatting with Grace from the San Francisco Bay Area.
+
+Here's how it works: at 8:00pm EDT`;
+const expectedSimple = `Hi Anna, you've got a match! On Wednesday you'll be chatting with Grace from the San Francisco Bay Area.
+
+Here's how it works: at 8:00pm PDT`;
+const expectedTwoMatches = `Hi Anna, we have two matches for you! On Wednesday you'll be chatting with Grace and on Thursday you'll be chatting with Rachael. They are both from the San Francisco Bay Area.
+
+Here's how it works: at 8:00pm PDT both nights`;
+const expectedTwoMatchesSameLocation = `Hi Anna, we have two matches for you! On Wednesday you'll be chatting with Grace and on Thursday you'll be chatting with Rachael. They are both from New York City.
+
+Here's how it works: at 8:00pm PDT both nights`;
+const expectedTwoMatchesDiffLocation = `Hi Anna, we have two matches for you! On Wednesday you'll be chatting with Grace from New York City and on Thursday you'll be chatting with Rachael from the San Francisco Bay Area.
+
+Here's how it works: at 8:00pm PDT both nights`;
 
 it("matchNotification for a single match - ET", async () => {
     const user1 = user("Anna");
@@ -39,11 +49,11 @@ it("matchNotification formats time according to each user's timezone", async () 
 
     const user1Res = matchNotification(userId1, [m], { [userId1]: user1, [userId2]: user2 })
     expect(user1Res).toHaveLength(1);
-    expect(user1Res[0]).toContain("At 7:00pm PDT")
+    expect(user1Res[0]).toContain("7:00pm PDT")
 
     const user2Res = matchNotification(userId2, [m], { [userId1]: user1, [userId2]: user2 })
     expect(user2Res).toHaveLength(1);
-    expect(user2Res[0]).toContain("At 9:00pm CDT")
+    expect(user2Res[0]).toContain("9:00pm CDT")
 });
 
 it("matchNotification correctly formats half past dates", async () => {
@@ -80,7 +90,7 @@ it("matchNotification for a single match - fun facts for both", async () => {
     const res = matchNotification(userId1, [m], { [userId1]: user1, [userId2]: user2 })
     expect(res).toHaveLength(2);
     expect(res[0]).toContain(expectedSimple)
-    expect(res[1]).toEqual(`Here are a few fun facts about Grace: "funFactsGrace"
+    expect(res[1]).toContain(`Here are a few fun facts about Grace: "funFactsGrace"
 
 Happy chatting!`)
 });
