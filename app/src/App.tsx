f64eafe3544755eb8f91b82e64c8150d8e444725
Grace Wang <gracewang92@gmail.com>
2021-04-03 15:01:27 -0700

    only use prod firebase config for prod lol

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -15,7 +15,7 @@ import VideoAvailability from "./scheduling/VideoAvailability";
 import Terms from "./Terms";
 import VideoRedirect from "./VideoRedirect";
 
-/*const firebaseConfig = process.env.PUBLIC_URL.startsWith("https://storydating.com")
+const firebaseConfig = process.env.URL?.startsWith("https://storydating.com")
   ? {
     apiKey: "AIzaSyATNRFSSQDVmI90c5y7FI817U9lWiH19_w",
     authDomain: "speakeasy-prod.firebaseapp.com",
@@ -35,18 +35,7 @@ import VideoRedirect from "./VideoRedirect";
     messagingSenderId: "232409000476",
     appId: "1:232409000476:web:bb1dafb10521d00aa14dd9",
     measurementId: "G-B4H5ZQQZBD",
-  };*/
-const firebaseConfig =
-{
-  apiKey: "AIzaSyATNRFSSQDVmI90c5y7FI817U9lWiH19_w",
-  authDomain: "speakeasy-prod.firebaseapp.com",
-  databaseURL: "https://speakeasy-prod.firebaseio.com",
-  projectId: "speakeasy-prod",
-  storageBucket: "speakeasy-prod.appspot.com",
-  messagingSenderId: "349979681156",
-  appId: "1:349979681156:web:3842ca3cf4b6381e21fdd1",
-  measurementId: "G-XG7FSYCC65",
-};
+  };
 
 firebase.initializeApp(firebaseConfig);
 
