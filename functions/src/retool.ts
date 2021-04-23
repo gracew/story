dae58ba6b09c613b71261c4c44f6438820d73ed8
Grace Wang <gracewang92@gmail.com>
2021-04-22 23:57:22 -0700

    fix last build error

--- functions/src/retool.ts
+++ functions/src/retool.ts
@@ -1,12 +1,11 @@
 
 import * as functions from "firebase-functions";
-import { createMatchFirestore } from "./csv";
 import { Firestore } from "./firestore";
 import { analyzeCollection as analyzeCollectionHelper } from "./validateMatches2";
 
 export const createMatch = functions.https.onRequest(
   async (request, response) => {
-    const match = await createMatchFirestore(request.body, new Firestore());
+    const match = await new Firestore().createMatch(request.body);
     response.send(match);
   }
 );
