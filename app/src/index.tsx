40430e269b078f08bf34f4216bd93de705ae0502
Grace Wang <gracewang92@gmail.com>
2021-04-25 13:29:29 -0700

    add sentry to capture FE errors

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -1,4 +1,6 @@
 import * as FullStory from '@fullstory/browser';
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
@@ -6,6 +8,16 @@ import './index.css';
 import * as serviceWorker from './serviceWorker';
 
 FullStory.init({ orgId: '11AD82' });
+Sentry.init({
+  dsn: "https://bbdb45a05aa64747bcdfc9559affc6ef@o579165.ingest.sentry.io/5735335",
+  integrations: [new Integrations.BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 0.5,
+});
+
 
 ReactDOM.render(
   <React.StrictMode>
