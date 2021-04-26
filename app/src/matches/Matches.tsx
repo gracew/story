37ba0cc3bfd077dd6ba927525fac4f2ac5e5974b
Sumeet Agarwal <sumeet.a@gmail.com>
2021-04-25 17:27:32 -0700

    wrap up again

--- app/src/matches/Matches.tsx
+++ app/src/matches/Matches.tsx
@@ -1,30 +1,33 @@
-import React, {useEffect, useState} from "react";
+import React from "react";
+// import React, {useEffect, useState} from "react";
 import {useHistory} from "react-router-dom";
-import {getUpcomingMatches, UpcomingMatch} from "../apiClient";
+import {getUpcomingMatches} from "../apiClient";
 import CenteredSpin from "../components/CenteredSpin";
 import {Pagination} from "antd";
 
 // TODO: WIP
 export default function Matches(): JSX.Element {
   const history = useHistory();
-  const [upcomingMatches, setUpcomingMatches] = useState<UpcomingMatch[]>();
+  // const [upcomingMatches, setUpcomingMatches] = useState<UpcomingMatch[]>();
   // const [pageIndex, setPageIndex] = useState<number>(0);
 
-  useEffect(() => {
-    (async () => {
-     setUpcomingMatches(await getUpcomingMatches());
-    })();
-  }, []);
+  return (<div>{"Hello"}</div>)
 
-  if (!upcomingMatches) {
-    return <CenteredSpin />;
-  }
-  if (upcomingMatches.length === 0) {
-    history.push("/profile")
-  }
-
-  return (
-    <Pagination total={upcomingMatches.length} pageSize={1} onChange={(pageNo) => {
-    }
-  } />);
+  // useEffect(() => {
+  //   (async () => {
+  //    setUpcomingMatches(await getUpcomingMatches());
+  //   })();
+  // }, []);
+  //
+  // if (!upcomingMatches) {
+  //   return <CenteredSpin />;
+  // }
+  // if (upcomingMatches.length === 0) {
+  //   history.push("/profile")
+  // }
+  //
+  // return (
+  //   <Pagination total={upcomingMatches.length} pageSize={1} onChange={(pageNo) => {
+  //   }
+  // } />);
 }
