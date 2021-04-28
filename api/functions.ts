gracew/story

commit 2249ee15f1a3079d7abd3d638de27bba82e13104

Upcoming matches WIP (#171)
* backend

* wip

* Fix build

* fix the merge conflicts

* fix ci

* wip

* gonna throw this away

* Revert "gonna throw this away"

This reverts commit f8f79994ea254de9e844790853f9688810142d5d.

* fix a couple issues

* wrap up

* wrap up again

app/src/apiClient.ts
@@ -1,10 +1,15 @@
 import firebase from "firebase";
+import { Endpoints, Resources } from "../../api/responses";
 
 export const NotFound = Symbol("NotFound");
 
-export async function getPreferences(userId?: string): Promise<Record<string, any> | typeof NotFound> {
+export async function getPreferences(
+  userId?: string
+): Promise<Record<string, any> | typeof NotFound> {
   try {
-    const res = await firebase.functions().httpsCallable("getPreferences")({ userId });
+    const res = await firebase.functions().httpsCallable("getPreferences")({
+      userId,
+    });
     return res.data;
   } catch (err) {
     if (err.code === "not-found") {
@@ -13,4 +18,10 @@ export async function getPreferences(userId?: string): Promise<Record<string, an
       throw err;
     }
   }
+}
+
+export async function getUpcomingMatches(): Promise<Resources.UpcomingMatch[]> {
+  const res = await firebase.functions().httpsCallable("getUpcomingMatches")();
+  const resp = res.data as Endpoints.GetUpcomingMatches;
+  return resp.upcomingMatches;
 }

functions/src/firestore.ts
@@ -2,258 +2,311 @@ import * as admin from "firebase-admin";
 import moment = require("moment-timezone");
 
 export interface ICalendarDate {
-    year: number,
-    month: number,
-    day: number,
+  year: number;
+  month: number;
+  day: number;
 }
 
 // TODO: i think we can unify IUser and IPreOnboarded user, keeping this the way it is for now for
 // type safety and clarity
 export type IPreOnboardedUser = Pick<
-    IUser,
-    "id" | "phone" | "status" | "locationFlexibility" | "onboardingComplete">;
+  IUser,
+  "id" | "phone" | "status" | "locationFlexibility" | "onboardingComplete"
+>;
 
 export interface IUser {
-    id: string;
-    firstName: string;
-    phone: string;
-    gender: string;
-    genderPreference: string;
-    age: number | string;
-    matchMin: number;
-    matchMax: number;
-    location: string;
-    timezone: string;
-    locationFlexibility?: boolean;
-    funFacts?: string;
-    status?: string;
-    photo?: string;
-    beta?: boolean;
-    // fields from onboarding
-    whereDidYouHearAboutUs?: string,
-    birthdate?: ICalendarDate,
-    pronouns?: string,
-    interests?: string,
-    social?: string,
-    onboardingComplete: boolean;
+  id: string;
+  firstName: string;
+  phone: string;
+  gender: string;
+  genderPreference: string;
+  age: number | string;
+  matchMin: number;
+  matchMax: number;
+  location: string;
+  timezone: string;
+  locationFlexibility?: boolean;
+  funFacts?: string;
+  status?: string;
+  photo?: string;
+  beta?: boolean;
+  // fields from onboarding
+  whereDidYouHearAboutUs?: string;
+  birthdate?: ICalendarDate;
+  pronouns?: string;
+  interests?: string;
+  social?: string;
+  onboardingComplete: boolean;
 }
 
 // TODO: incomplete
 export interface IPreferences {
-    connectionType?: { value: string }
+  connectionType?: { value: string };
 }
 
 export interface IMatch {
-    id: string;
-    user_a_id: string;
-    user_a_revealed?: boolean;
-    user_b_id: string;
-    user_b_revealed?: boolean;
-    user_ids: string[];
-    joined?: Record<string, boolean>,
-    created_at: admin.firestore.Timestamp;
-    canceled?: boolean;
-    rescheduled?: boolean;
-    interactions: {
-        notified?: boolean;
-        reminded?: boolean;
-        called?: boolean;
-        recalled?: boolean;
-        flakesHandled?: boolean;
-        warned5Min?: boolean;
-        warned1Min?: boolean;
-        revealRequested?: boolean;
-        // if both respond Y to the phone call, have we notified both users of the next step
-        nextStepHandled?: boolean;
-    },
-    mode?: string;
+  id: string;
+  user_a_id: string;
+  user_a_revealed?: boolean;
+  user_b_id: string;
+  user_b_revealed?: boolean;
+  user_ids: string[];
+  joined?: Record<string, boolean>;
+  created_at: admin.firestore.Timestamp;
+  canceled?: boolean;
+  rescheduled?: boolean;
+  interactions: {
+    notified?: boolean;
+    reminded?: boolean;
+    called?: boolean;
+    recalled?: boolean;
+    flakesHandled?: boolean;
+    warned5Min?: boolean;
+    warned1Min?: boolean;
+    revealRequested?: boolean;
+    // if both respond Y to the phone call, have we notified both users of the next step
+    nextStepHandled?: boolean;
+  };
+  mode?: "video" | "phone";
 
-    // used for phone matches
-    twilioSid?: string;
-    recordingOverride?: boolean;
-    ongoing?: boolean;  // whether at least 1  person is on the call
+  // used for phone matches
+  twilioSid?: string;
+  recordingOverride?: boolean;
+  ongoing?: boolean; // whether at least 1  person is on the call
 
-    // used for video matches
-    videoId?: string;
-    videoLink?: string;
-    videoPasscode?: string;
-    videoAvailability?: Record<string, any>;
+  // used for video matches
+  videoId?: string;
+  videoLink?: string;
+  videoPasscode?: string;
+  videoAvailability?: Record<string, any>;
 }
 
 interface ISchedulingRecord {
-    interactions: {
-        reminded: boolean;
-        requested: boolean;
-        responded: boolean;
-    }
+  interactions: {
+    reminded: boolean;
+    requested: boolean;
+    responded: boolean;
+  };
 
-    // the following fields are set if interactions.responded = true
-    available?: admin.firestore.Timestamp[],
-    matches?: number;
-    skip?: boolean;
+  // the following fields are set if interactions.responded = true
+  available?: admin.firestore.Timestamp[];
+  matches?: number;
+  skip?: boolean;
 }
 
-export function timestamp(isoDate: string) {
-    return new admin.firestore.Timestamp(new Date(isoDate).getTime() / 1000, 0);
+export function toFirestoreTimestamp(d: Date) {
+  return new admin.firestore.Timestamp(d.getTime() / 1000, 0);
 }
 
 export interface CreateMatchInput {
-    userAId: string;
-    userBId: string;
-    time: string;
-    canceled?: boolean;
-    mode?: string;
+  userAId: string;
+  userBId: string;
+  time: Date;
+  canceled?: boolean;
+  mode?: "video" | "phone";
 }
 
 export class Firestore {
-    public async saveUser(user: IUser | IPreOnboardedUser): Promise<void> {
-        await admin.firestore().collection("users").doc(user.id).update(user);
+  public async saveUser(user: IUser | IPreOnboardedUser): Promise<void> {
+    await admin.firestore().collection("users").doc(user.id).update(user);
+  }
+
+  public async setPreferences(
+    userId: string,
+    prefs: IPreferences
+  ): Promise<void> {
+    await admin.firestore().collection("preferences").doc(userId).set(prefs);
+  }
+
+  public async getOrCreateUser(phone: string): Promise<IPreOnboardedUser> {
+    const existingUser = await this.getUserByPhone(phone);
+    if (existingUser) {
+      return existingUser;
+    }
+    // create record
+    const doc = admin.firestore().collection("users").doc();
+    const data = {
+      id: doc.id,
+      phone,
+      registeredAt: admin.firestore.FieldValue.serverTimestamp(),
+      eligible: true,
+      status: "waitlist",
+      onboardingComplete: false,
+      locationFlexibility: true,
+    };
+    await doc.create(data);
+    return data;
+  }
+
+  public async getUser(id: string): Promise<IUser | undefined> {
+    const user = await admin.firestore().collection("users").doc(id).get();
+    return user.data() as IUser | undefined;
+  }
+
+  public async getUserByPhone(phone: string): Promise<IUser | undefined> {
+    const users = await admin.firestore().collection("users");
+    const user = await users.where("phone", "==", phone).get();
+    if (user.empty) {
+      return;
+    }
+    return user.docs[0].data() as IUser;
+  }
+
+  public async getMatch(id: string): Promise<IMatch | undefined> {
+    const match = await admin.firestore().collection("matches").doc(id).get();
+    return match.data() as IMatch | undefined;
+  }
+
+  public async createMatch(params: CreateMatchInput): Promise<IMatch> {
+    const userA = await this.getUser(params.userAId);
+    const userB = await this.getUser(params.userBId);
+
+    if (!userA) {
+      throw new Error("unknown user id " + params.userAId);
+    }
+    if (!userB) {
+      throw new Error("unknown user id " + params.userBId);
+    }
+    const ref = admin.firestore().collection("matches").doc();
+    const match = {
+      id: ref.id,
+      user_a_id: params.userAId,
+      user_b_id: params.userBId,
+      user_ids: [params.userAId, params.userBId],
+      joined: {},
+      created_at: toFirestoreTimestamp(params.time),
+      canceled: params.canceled || false,
+      interactions: {
+        notified: false,
+        reminded: false,
+        called: false,
+        recalled: false,
+        flakesHandled: false,
+        warned5Min: false,
+        warned1Min: false,
+        revealRequested: false,
+      },
+      mode: params.mode || "phone",
+    };
+    await ref.set(match);
+    return match;
+  }
+
+  public async rescheduleMatch(id: string, newTime: Date) {
+    return this.updateMatch(id, {
+      rescheduled: true,
+      created_at: toFirestoreTimestamp(newTime),
+    });
+  }
+
+  public cancelMatch(id: string) {
+    return admin
+      .firestore()
+      .collection("matches")
+      .doc(id)
+      .update("canceled", true);
+  }
+
+  public async currentMatchForUser(id: string): Promise<IMatch | undefined> {
+    const createdAt = moment().utc().startOf("hour");
+    if (moment().minutes() >= 30) {
+      createdAt.add(30, "minutes");
+    }
+    const match = await admin
+      .firestore()
+      .collection("matches")
+      .where("user_ids", "array-contains", id)
+      .where("created_at", "==", createdAt)
+      .where("canceled", "==", false)
+      .get();
+    return match.empty ? undefined : (match.docs[0].data() as IMatch);
+  }
+
+  // ordered by meeting time, ascended, where all matches have a meeting time after now.
+  public async upcomingMatchesForUser(userId: string): Promise<IMatch[]> {
+    const querySnapshot = await admin
+      .firestore()
+      .collection("matches")
+      .where("user_ids", "array-contains", userId)
+      .where("canceled", "==", false)
+      // created_at is actually their meeting time, not the time the record was created
+      .where("created_at", ">=", new Date())
+      .orderBy("created_at", "asc")
+      .get();
+    return querySnapshot.docs.map((snap) => snap.data() as IMatch);
+  }
+
+  public async nextMatchForUser(userId: string): Promise<IMatch | undefined> {
+    return (await this.upcomingMatchesForUser(userId))[0];
+  }
+
+  public async getUsersForMatches(
+    matches: IMatch[]
+  ): Promise<Record<string, IUser>> {
+    if (matches.length === 0) {
+      return {};
     }
 
-    public async setPreferences(userId: string, prefs: IPreferences): Promise<void> {
-        await admin.firestore().collection("preferences").doc(userId).set(prefs);
+    const userARefs = matches.map((m) =>
+      admin.firestore().collection("users").doc(m.user_a_id)
+    );
+    const userBRefs = matches.map((m) =>
+      admin.firestore().collection("users").doc(m.user_b_id)
+    );
+
+    const allUsers = await admin
+      .firestore()
+      .getAll(...userARefs.concat(userBRefs));
+    return Object.assign(
+      {},
+      ...allUsers.map((user) => ({ [user.id]: user.data() }))
+    );
+  }
+
+  public async createSchedulingRecords(week: string, userIds: string[]) {
+    const batch = admin.firestore().batch();
+    const data = {
+      interactions: {
+        requested: false, // we requested the user's availability
+        responded: false, // the user provided their availability
+        reminded: false,
+      },
+    };
+    userIds.forEach((userId) => {
+      batch.create(
+        admin
+          .firestore()
+          .collection("scheduling")
+          .doc(week)
+          .collection("users")
+          .doc(userId),
+        data
+      );
+    });
+    return batch.commit();
+  }
+
+  public async getSchedulingRecords(
+    week: string,
+    userIds: string[]
+  ): Promise<Record<string, ISchedulingRecord>> {
+    if (userIds.length === 0) {
+      return {};
     }
+    const userRefs = userIds.map((id) =>
+      admin
+        .firestore()
+        .collection("scheduling")
+        .doc(week)
+        .collection("users")
+        .doc(id)
+    );
+    const res = await admin.firestore().getAll(...userRefs);
+    return Object.assign({}, ...res.map((doc) => ({ [doc.id]: doc.data() })));
+  }
 
-    public async getOrCreateUser(phone: string): Promise<IPreOnboardedUser> {
-        const existingUser = await this.getUserByPhone(phone);
-        if (existingUser) {
-            return existingUser;
-        }
-        // create record
-        const doc = admin.firestore().collection("users").doc();
-        const data = {
-            id: doc.id,
-            phone,
-            registeredAt: admin.firestore.FieldValue.serverTimestamp(),
-            eligible: true,
-            status: "waitlist",
-            onboardingComplete: false,
-            locationFlexibility: true,
-        };
-        await doc.create(data);
-        return data;
-    }
-
-    public async getUser(id: string): Promise<IUser | undefined> {
-        const user = await admin.firestore().collection("users").doc(id).get();
-        return user.data() as IUser | undefined;
-    }
-
-    public async getUserByPhone(phone: string): Promise<IUser | undefined> {
-        const users = await admin.firestore().collection("users");
-        const user = await users.where("phone", "==", phone).get();
-        if (user.empty) {
-            return;
-        }
-        return user.docs[0].data() as IUser;
-    }
-
-    public async getMatch(id: string): Promise<IMatch | undefined> {
-        const match = await admin.firestore().collection("matches").doc(id).get();
-        return match.data() as IMatch | undefined;
-    }
-
-    public async createMatch(data: CreateMatchInput) {
-        const userA = await this.getUser(data.userAId);
-        const userB = await this.getUser(data.userBId);
-
-        if (!userA) {
-            console.error(new Error("unknown user id " + data.userAId));
-            return;
-        }
-        if (!userB) {
-            console.error(new Error("unknown user id " + data.userBId));
-            return;
-        }
-        const match = {
-            user_a_id: data.userAId,
-            user_b_id: data.userBId,
-            user_ids: [data.userAId, data.userBId],
-            joined: {},
-            created_at: timestamp(data.time),
-            canceled: data.canceled || false,
-            interactions: {
-                notified: false,
-                reminded: false,
-                called: false,
-                recalled: false,
-                flakesHandled: false,
-                warned5Min: false,
-                warned1Min: false,
-                revealRequested: false,
-            },
-            mode: data.mode || "phone",
-        };
-        const ref = admin.firestore().collection("matches").doc();
-        await ref.set({ ...match, id: ref.id });
-        return match;
-    }
-
-    public updateMatch(id: string, update: Partial<IMatch>) {
-        return admin.firestore().collection("matches").doc(id).update(update);
-    }
-
-    public cancelMatch(id: string) {
-        return admin.firestore().collection("matches").doc(id).update("canceled", true);
-    }
-
-    public async currentMatchForUser(id: string): Promise<IMatch | undefined> {
-        const createdAt = moment().utc().startOf("hour");
-        if (moment().minutes() >= 30) {
-            createdAt.add(30, "minutes");
-        }
-        const match = await admin.firestore().collection("matches")
-            .where("user_ids", "array-contains", id)
-            .where("created_at", "==", createdAt)
-            .where("canceled", "==", false)
-            .get();
-        return match.empty ? undefined : match.docs[0].data() as IMatch;
-    }
-
-    public async nextMatchForUser(id: string): Promise<IMatch | undefined> {
-        const latestMatchOther = await admin.firestore().collection("matches")
-            .where("user_ids", "array-contains", id)
-            .where("canceled", "==", false)
-            .where("created_at", ">=", new Date())
-            .orderBy("created_at", "asc")
-            .limit(1)
-            .get();
-        return latestMatchOther.empty ? undefined : latestMatchOther.docs[0].data() as IMatch;
-    }
-
-    public async getUsersForMatches(matches: IMatch[]): Promise<Record<string, IUser>> {
-        if (matches.length === 0) {
-            return {};
-        }
-
-        const userARefs = matches.map(m => admin.firestore().collection("users").doc(m.user_a_id));
-        const userBRefs = matches.map(m => admin.firestore().collection("users").doc(m.user_b_id));
-
-        const allUsers = await admin.firestore().getAll(...userARefs.concat(userBRefs));
-        return Object.assign({}, ...allUsers.map(user => ({ [user.id]: user.data() })));
-    }
-
-    public async createSchedulingRecords(week: string, userIds: string[]) {
-        const batch = admin.firestore().batch();
-        const data = {
-            interactions: {
-                requested: false,   // we requested the user's availability
-                responded: false,   // the user provided their availability
-                reminded: false,
-            },
-        }
-        userIds.forEach(userId => {
-            batch.create(admin.firestore().collection("scheduling").doc(week).collection("users").doc(userId), data);
-        })
-        return batch.commit();
-    }
-
-    public async getSchedulingRecords(week: string, userIds: string[]): Promise<Record<string, ISchedulingRecord>> {
-        if  (userIds.length === 0) {
-            return {};
-        }
-        const userRefs = userIds.map(id => admin.firestore().collection("scheduling").doc(week).collection("users").doc(id));
-        const res = await admin.firestore().getAll(...userRefs);
-        return Object.assign({}, ...res.map(doc => ({ [doc.id]: doc.data() })))
-    }
+  // TODO: let's make this private to decouple callers from inner workings of firestore
+  public updateMatch(id: string, update: Partial<IMatch>) {
+    return admin.firestore().collection("matches").doc(id).update(update);
+  }
 }

functions/src/retool.ts
@@ -1,11 +1,12 @@
-
 import * as functions from "firebase-functions";
-import { Firestore } from "./firestore";
+import { CreateMatchInput, Firestore } from "./firestore";
 import { analyzeCollection as analyzeCollectionHelper } from "./validateMatches2";
 
+// TODO: add a match response for API
+// possibly validate that the match time is allowed?
 export const createMatch = functions.https.onRequest(
-  async (request, response) => {
-    const match = await new Firestore().createMatch(request.body);
+  async ({ body }: { body: CreateMatchInput }, response) => {
+    const match = await new Firestore().createMatch(body);
     response.send(match);
   }
 );

api/README
@@ -1,0 +1,1 @@
+Shared API types between frontend and backend

api/responses.ts
@@ -1,0 +1,22 @@
+export namespace Endpoints {
+  export interface GetUpcomingMatches {
+    upcomingMatches: Resources.UpcomingMatch[];
+  }
+}
+
+export namespace Resources {
+  export interface UpcomingMatch {
+    firstName: string;
+    // photo is undefined if they haven't been "revealed" yet... i.e., they haven't had their phone meeting yet
+    photo?: string;
+    funFacts: string;
+    meetingTime: Types.JSONDateTime;
+    mode: Types.MatchMode;
+  }
+}
+
+export namespace Types {
+  // via Date.prototype.toJSON
+  export type JSONDateTime = string;
+  export type MatchMode = "video" | "phone";
+}

app/src/App.tsx
@@ -16,6 +16,7 @@ import PublicProfile from "./profile/PublicProfile";
 import VideoAvailability from "./scheduling/VideoAvailability";
 import Terms from "./Terms";
 import VideoRedirect from "./VideoRedirect";
+// import Matches from "./matches/Matches";
 
 const firebaseConfig = process.env.PUBLIC_URL.startsWith("https://storydating.com")
   ? {
@@ -45,6 +46,9 @@ function App() {
   if (process.env.NODE_ENV === "development" && !process.env.REACT_APP_STAGING_DEV) {
     firebase.functions().useEmulator("localhost", 5001);
     firebase.auth().useEmulator("http://localhost:9099");
+    // @ts-ignore -- this is really handy for debugging, since we can access window.firebase from
+    // the browser console
+    window.firebase = firebase;
   }
   firebase.remoteConfig().fetchAndActivate();
   firebase.analytics();
@@ -71,6 +75,11 @@ function App() {
               <Header />
               <PublicProfile />
             </Route>
+            {/*TODO: this route is WIP*/}
+            {/*<PrivateRoute path={["/m", "/matches"]}>*/}
+            {/*  <Header showLogout={true} />*/}
+            {/*  <Matches />*/}
+            {/*</PrivateRoute>*/}
             <PrivateRoute path="/m/:matchId">
               <Header showLogout={true} />
               <VideoAvailability />

app/src/matches/Matches.tsx
@@ -1,0 +1,33 @@
+import React from "react";
+// import React, {useEffect, useState} from "react";
+import {useHistory} from "react-router-dom";
+import {getUpcomingMatches} from "../apiClient";
+import CenteredSpin from "../components/CenteredSpin";
+import {Pagination} from "antd";
+
+// TODO: WIP
+export default function Matches(): JSX.Element {
+  const history = useHistory();
+  // const [upcomingMatches, setUpcomingMatches] = useState<UpcomingMatch[]>();
+  // const [pageIndex, setPageIndex] = useState<number>(0);
+
+  return (<div>{"Hello"}</div>)
+
+  // useEffect(() => {
+  //   (async () => {
+  //    setUpcomingMatches(await getUpcomingMatches());
+  //   })();
+  // }, []);
+  //
+  // if (!upcomingMatches) {
+  //   return <CenteredSpin />;
+  // }
+  // if (upcomingMatches.length === 0) {
+  //   history.push("/profile")
+  // }
+  //
+  // return (
+  //   <Pagination total={upcomingMatches.length} pageSize={1} onChange={(pageNo) => {
+  //   }
+  // } />);
+}

commit 224bbd4b9e7e102a7011a2be76d70450b44b7f3b

fix broken createMatch endpoint (#172)
crap... this was broken by https://github.com/gracew/story/pull/171

quick fix, only manual testing.

api/functions.ts
@@ -1,4 +1,14 @@
-export namespace Endpoints {
+export namespace Requests {
+  export interface CreateMatch {
+    userAId: string;
+    userBId: string;
+    time: Types.JSONDateTime;
+    canceled?: boolean;
+    mode?: Types.MatchMode;
+  }
+}
+
+export namespace Responses {
   export interface GetUpcomingMatches {
     upcomingMatches: Resources.UpcomingMatch[];
   }
@@ -18,5 +28,6 @@ export namespace Resources {
 export namespace Types {
   // via Date.prototype.toJSON
   export type JSONDateTime = string;
+  // TODO: make this an enum
   export type MatchMode = "video" | "phone";
 }

app/src/apiClient.ts
@@ -1,5 +1,5 @@
 import firebase from "firebase";
-import { Endpoints, Resources } from "../../api/responses";
+import { Responses, Resources } from "../../api/functions";
 
 export const NotFound = Symbol("NotFound");
 
@@ -22,6 +22,6 @@ export async function getPreferences(
 
 export async function getUpcomingMatches(): Promise<Resources.UpcomingMatch[]> {
   const res = await firebase.functions().httpsCallable("getUpcomingMatches")();
-  const resp = res.data as Endpoints.GetUpcomingMatches;
+  const resp = res.data as Responses.GetUpcomingMatches;
   return resp.upcomingMatches;
 }

functions/src/app.ts
@@ -4,14 +4,14 @@ import { CallableContext } from "firebase-functions/lib/providers/https";
 import { isEmpty } from "lodash";
 import * as moment from "moment-timezone";
 import fetch from "node-fetch";
-import { Endpoints } from "../../api/responses";
 import { createSmsChatHelper } from "./calls";
+import { Responses } from "../../api/functions";
 import {
   CreateMatchInput,
   Firestore,
   IMatch,
   IPreferences,
-  IUser
+  IUser,
 } from "./firestore";
 import { listUpcomingMatchViewsForUser } from "./matches";
 import { findCommonAvailability } from "./scheduling";
@@ -21,7 +21,7 @@ import {
   videoFallbackSwapNumbers,
   videoFallbackTextChat,
   videoMatchNotification,
-  welcome
+  welcome,
 } from "./smsCopy";
 import { processTimeZone, Timezone, videoTimeOptions } from "./times";
 import { nextMatchNameAndDate, sendSms } from "./twilio";
@@ -330,7 +330,7 @@ function timezone(location: string) {
 }
 
 export const getUpcomingMatches = functions.https.onCall(
-  async (data, context): Promise<Endpoints.GetUpcomingMatches> => {
+  async (data, context): Promise<Responses.GetUpcomingMatches> => {
     const user = await requireLoggedInUser(data, context);
     const matchViews = await listUpcomingMatchViewsForUser(user);
     return {

functions/src/firestore.ts
@@ -94,10 +94,6 @@ interface ISchedulingRecord {
   skip?: boolean;
 }
 
-export function toFirestoreTimestamp(d: Date) {
-  return new admin.firestore.Timestamp(d.getTime() / 1000, 0);
-}
-
 export interface CreateMatchInput {
   userAId: string;
   userBId: string;
@@ -158,6 +154,7 @@ export class Firestore {
     return match.data() as IMatch | undefined;
   }
 
+  // TODO: possibly prevent duplicates here?
   public async createMatch(params: CreateMatchInput): Promise<IMatch> {
     const userA = await this.getUser(params.userAId);
     const userB = await this.getUser(params.userBId);
@@ -175,7 +172,7 @@ export class Firestore {
       user_b_id: params.userBId,
       user_ids: [params.userAId, params.userBId],
       joined: {},
-      created_at: toFirestoreTimestamp(params.time),
+      created_at: admin.firestore.Timestamp.fromDate(params.time),
       canceled: params.canceled || false,
       interactions: {
         notified: params.notified || false,
@@ -196,7 +193,7 @@ export class Firestore {
   public async rescheduleMatch(id: string, newTime: Date) {
     return this.updateMatch(id, {
       rescheduled: true,
-      created_at: toFirestoreTimestamp(newTime),
+      created_at: admin.firestore.Timestamp.fromDate(newTime),
     });
   }
 

functions/src/retool.ts
@@ -1,12 +1,16 @@
 import * as functions from "firebase-functions";
-import { CreateMatchInput, Firestore } from "./firestore";
+import { Firestore } from "./firestore";
 import { analyzeCollection as analyzeCollectionHelper } from "./validateMatches2";
+import { Requests } from "../../api/functions";
 
 // TODO: add a match response for API
 // possibly validate that the match time is allowed?
 export const createMatch = functions.https.onRequest(
-  async ({ body }: { body: CreateMatchInput }, response) => {
-    const match = await new Firestore().createMatch(body);
+  async ({ body }: { body: Requests.CreateMatch }, response) => {
+    const match = await new Firestore().createMatch({
+      ...body,
+      time: new Date(body.time),
+    });
     response.send(match);
   }
 );