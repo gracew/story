bc842398342084b861dd791cd18da76cd453fe95
Grace Wang <gracewang92@gmail.com>
2021-06-03 22:40:06 -0400

    indicate selected item in menu with underline

--- app/src/Header.tsx
+++ app/src/Header.tsx
@@ -1,14 +1,27 @@
 import { HeartFilled, UserOutlined } from "@ant-design/icons";
-import { Button } from "antd";
+import { Button, Menu } from "antd";
 import firebase from "firebase";
-import React, { useState } from "react";
-import { useHistory } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { useHistory, useLocation } from "react-router-dom";
 import logo from './assets/mainlogo.svg';
 import "./Header.css";
 
+enum Page {
+  MATCHES = "matches",
+  PROFILE = "profile",
+}
+
 function Header() {
   const history = useHistory();
+  const location = useLocation();
   const [loggedIn, setLoggedIn] = useState(false);
+  const [currentPage, setCurrentPage] = useState(Page.PROFILE);
+
+  useEffect(() => {
+    if (["/matches", "/m"].includes(location.pathname)) {
+      setCurrentPage(Page.MATCHES);
+    }
+  }, [location.pathname])
 
   firebase.auth().onAuthStateChanged(function (user) {
     if (user) {
@@ -16,28 +29,38 @@ function Header() {
     }
   });
 
+  function onMenuClick(e: any) {
+    const p = e.key as Page;
+    setCurrentPage(p);
+    switch (p) {
+      case Page.MATCHES:
+        history.push("/matches");
+        break;
+      case Page.PROFILE:
+      default:
+        history.push("/profile");
+        break;
+    }
+  }
+
   return (
     <div className="story-header">
       <Button type="link" onClick={() => history.push("/profile")}>
         <img src={logo} alt="story-logo" />
       </Button>
       {loggedIn &&
-        <div>
-          <Button
-            className="menu-button"
-            type="text"
+        <Menu onClick={onMenuClick} selectedKeys={[currentPage]} mode="horizontal">
+          <Menu.Item
+            key={Page.MATCHES}
+            className="story-header-menu-item"
             icon={<HeartFilled />}
-            size="large"
-            onClick={() => history.push("/matches")}
           />
-          <Button
-            className="menu-button"
-            type="text"
+          <Menu.Item
+            key={Page.PROFILE}
+            className="story-header-menu-item"
             icon={<UserOutlined />}
-            size="large"
-            onClick={() => history.push("/profile")}
           />
-        </div>
+        </Menu>
       }
     </div>
   );
